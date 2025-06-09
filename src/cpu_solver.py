a1e641c63a89750ada3a06bf4f66c6ce406c77ca
nando-2002 <prthik.kar@gmail.com>
2025-06-09 17:40:20 +0530

    minor updates

--- src/cpu_solver.py
+++ src/cpu_solver.py
@@ -2,27 +2,44 @@ import numpy as np
 from numba import jit, prange
 
 @jit(parallel = True, nopython = True)
-def bracket_terms(P, NX, DX, UN, VN, DT, rho):
+def bracket_terms(NX, DX, UN, VN, DT, rho):
 
     DY = DX
-    output = np.zeros(NX, NX)
+    NY = NX
+    output = np.zeros((NX, NX))
     for i in prange(NX):
-        for j in prange(NX):
-            output = (
+        for j in prange(NY):
+            output[i][j] = (
             (
                 rho * (DX**2) * (DY**2) / (2*(DX**2 + DY**2))
             )  
             *
             (
+            (
                 1/DT
             )
             *
             (
-                UN[]
+                (UN[i + 1][j] - UN[i - 1][j])/(2*DX) + (VN[i][j + 1] - VN[i][j - 1])/(2*DY)
             )
-    )
+            -
+            (
+                ((UN[i + 1][j] - UN[i - 1][j])/(2*DX))**2
+            )
+            -
+            (
+                2*((UN[i][j + 1] - UN[i][j - 1])/(2*DY))*((VN[i + 1][j] - VN[i - 1][j])/(2*DX))
+            )
+            -
+            (
+                ((VN[i][j + 1] - VN[i][j - 1])/(2*DY))**2
+            )
+            )
+            )
+        
+    return output
 
-
+@jit(nopython = True, nogil = True)
 def pressure_poisson():
     pass
 
@@ -41,6 +58,7 @@ def solve_with_cpu(NX, DT, NT, nu, rho, poisIT):
     VEL_NEXT_V = np.zeros((NX, NX))
     PRES_NEXT = np.zeros((NX, NX))
 
-    P_BRACKET = bracket_terms(PRES_NOW, NX, (x[1] - x[0]), VEL_NOW_U, VEL_NOW_V, DT, rho)
+    for i in range(NT):
+       P_BRACKET = bracket_terms(NX, (x[1] - x[0]), VEL_NOW_U, VEL_NOW_V, DT, rho)
 
     #main loop goes here
