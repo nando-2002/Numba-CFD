24d4455a8c537070e2324d291d52b734aa4350d5
unknown <prthik.kar@gmail.com>
2025-06-11 09:48:47 +0530

    updates

--- src/cpu_solver.py
+++ src/cpu_solver.py
@@ -1,14 +1,14 @@
 import numpy as np 
 from numba import jit, prange
 
-@jit(parallel = True, nopython = True)
+@jit(nopython = True, nogil = True, parallel = False)
 def bracket_terms(NX, DX, UN, VN, DT, rho):
 
     DY = DX
     NY = NX
     output = np.zeros((NX, NX))
-    for i in prange(NX):
-        for j in prange(NY):
+    for i in prange(1, NX - 1, 1):
+        for j in prange(1, NY - 1, 1):
             output[i][j] = (
             (
                 rho * (DX**2) * (DY**2) / (2*(DX**2 + DY**2))
@@ -39,26 +39,152 @@ def bracket_terms(NX, DX, UN, VN, DT, rho):
         
     return output
 
-@jit(nopython = True, nogil = True)
-def pressure_poisson():
-    pass
+@jit(nopython = True, nogil = True, parallel = False)
+def pressure_poisson(P, DX, P_BRACKET, poisIT, NX):
+
+    DY = DX
+    NY = NX
+
+    for k in range(poisIT):
+        P_COPY = P.copy()
+        for i in prange(1, NX - 1, 1):
+            for j in prange(1, NY - 1, 1):
+                P[i][j] = (
+                    (
+                    (
+                        (P_COPY[i + 1][j] + P_COPY[i - 1][j])*DY**2
+                    )
+                    +
+                    (
+                        (P_COPY[i][j + 1] + P_COPY[i][j - 1])*DX**2
+                    )
+                    )
+                    /
+                    (
+                        2*(DX**2 + DY**2)
+                    )
+                    -
+                    (
+                        P_BRACKET[i][j]
+                    )
+                    )
+        
+        P[:, -1] = P[:, -2] # dp/dx = 0 at x = 1
+        P[0, :] = P[1, :]   # dp/dy = 0 at y = 0
+        P[:, 0] = P[:, 1]   # dp/dx = 0 at x = 0
+        P[-1, :] = 0       # p = 0 at y = 1
+    
+    return P
 
 
-@jit(nopython = True, nogil = True, parallel = True)
-def solve_with_cpu(NX, DT, NT, nu, rho, poisIT):
+@jit(nopython = True, nogil = True, parallel = False)
+def solve(NX, DT, NT, nu, rho, poisIT):
 
-    x = np.linspace(0, 1, NX - 1)
-    y = np.linspace(0, 1, NX - 1)
+    NY = NX
+    x = np.linspace(0, 1, NX)
+    y = np.linspace(0, 1, NX)
+    DX = x[1] - x[0]
+    DY = y[1] - y[0]
 
     VEL_NOW_U = np.zeros((NX, NX))
     VEL_NOW_V= np.zeros((NX, NX))
     PRES_NOW = np.zeros((NX, NX))
 
     VEL_NEXT_U = np.zeros((NX, NX))
-    VEL_NEXT_V = np.zeros((NX, NX)) 
-    PRES_NEXT = np.zeros((NX, NX))
+    VEL_NEXT_V = np.zeros((NX, NX))
 
-    for i in range(NT):
-       P_BRACKET = bracket_terms(NX, (x[1] - x[0]), VEL_NOW_U, VEL_NOW_V, DT, rho)
+    for k in range(NT):
+        VEL_NOW_U = VEL_NEXT_U.copy()
+        VEL_NOW_V = VEL_NEXT_V.copy()
+        
+        P_BRACKET = bracket_terms(NX, (x[1] - x[0]), VEL_NOW_U, VEL_NOW_V, DT, rho)
+        PRES_NOW = pressure_poisson(PRES_NOW, (x[1] - x[0]), P_BRACKET, poisIT, NX)
 
-    #main loop goes here
+        for i in prange(1, NX - 1, 1):
+            for j in prange(1, NY - 1, 1):   
+               VEL_NEXT_U[i][j] = (
+               (
+                    VEL_NOW_U[i][j]
+               )
+               +
+               (
+                   - VEL_NOW_U[i][j]*(DT/DX)*(VEL_NOW_U[i][j] - VEL_NOW_U[i - 1][j])
+               )
+               +
+               (
+                   - VEL_NOW_V[i][j]*(DT/DY)*(VEL_NOW_U[i][j] - VEL_NOW_U[i][j - 1])
+               )
+               +
+               (
+                   (-DT/(rho*2*DX))*(PRES_NOW[i + 1][j] - PRES_NOW[i - 1][j]) 
+               )
+               +
+               (
+                   nu
+                   *
+                   (
+                       (DT/DX**2)
+                       *
+                       (
+                           VEL_NOW_U[i + 1][j] - 2*VEL_NOW_U[i][j] + VEL_NOW_U[i - 1][j]
+                       )
+                       +
+                       (DT/DX**2)
+                       *
+                       (
+                           VEL_NOW_U[i][j + 1] - 2*VEL_NOW_U[i][j] + VEL_NOW_U[i][j - 1]
+                       )
+                   )
+               )
+               )
+
+               VEL_NEXT_V[i][j] = (
+               (
+                    VEL_NOW_V[i][j]
+               )
+               +
+               (
+                   - VEL_NOW_U[i][j]*(DT/DX)*(VEL_NOW_V[i][j] - VEL_NOW_V[i - 1][j])
+               )
+               +
+               (
+                   - VEL_NOW_V[i][j]*(DT/DY)*(VEL_NOW_V[i][j] - VEL_NOW_V[i][j - 1])
+               )
+               +
+               (
+                   (-DT/(rho*2*DX))*(PRES_NOW[i][j + 1] - PRES_NOW[i][j - 1]) 
+               )
+               +
+               (
+                   nu
+                   *
+                   (
+                       (DT/DX**2)
+                       *
+                       (
+                           VEL_NOW_V[i + 1][j] - 2*VEL_NOW_V[i][j] + VEL_NOW_V[i - 1][j]
+                       )
+                       +
+                       (DT/DX**2)
+                       *
+                       (
+                           VEL_NOW_V[i][j + 1] - 2*VEL_NOW_V[i][j] + VEL_NOW_V[i][j - 1]
+                       )
+                   )
+               )
+               )
+               
+        VEL_NEXT_U[0, :]  = 0
+        VEL_NEXT_U[:, 0]  = 0
+        VEL_NEXT_U[:, -1] = 0
+        VEL_NEXT_U[-1, :] = 1   # set velocity on cavity lid equal to 1
+        VEL_NEXT_V[0, :]  = 0
+        VEL_NEXT_V[-1, :] = 0
+        VEL_NEXT_V[:, 0]  = 0
+        VEL_NEXT_V[:, -1] = 0
+
+    return PRES_NOW, VEL_NEXT_U, VEL_NEXT_V
+
+               
+
+#solve(41, 0.001, 100, 0.1, 1, 50)
--- src/main.py
+++ src/main.py
@@ -1,43 +1,57 @@
 import cpu_solver as cpu
 import gpu_solver as gpu
 
+import numpy
+from matplotlib import pyplot, cm
+import time
+
 def main():
 
     #config begins here
 
-    number_of_grid_steps = 41               #square grid, therefore x and y step counts are always the same
-    time_step_size = 0.01                   #seconds
-    time_step_count = 500                   #time_step_size * time_step_count = total simulation time
-    kinematic_viscosity = 0.1               #kinematic viscosity m^2/s
-    horiz_velocity = 1                      #m/s
-    density = 1                             #kg/m^3
+    NX = 41                   #square grid, therefore x and y step counts are always the same
+    DT = 0.001                   #seconds
+    NT = 100                  #DT * NT = total simulation time
+    nu = 0.1                    #kinematic viscosity m^2/s
+    hVEL = 1                    #m/s
+    rho = 1                     #kg/m^3
     
-    poisson_iterations = 200                #internal iterations to calculate pressre
+    poisson_iterations = 500   #internal iterations to calculate pressre
 
-    nvidia_gpu_present = False              #if nvidia gpu present, with cudatoolkits installed, we can run it on the gpu
+    nvidia_gpu_present = False  #if nvidia gpu present, with cudatoolkits installed, we can run it on the gpu
     
     if nvidia_gpu_present == True:
-        gpu.solve_with_gpu(
-            number_of_grid_steps, 
-            time_step_size, 
-            time_step_count, 
-            kinematic_viscosity, 
-            horiz_velocity, 
-            density, 
-            poisson_iterations
-            )
+        gpu.solve(NX, DT, NT, nu, hVEL, rho, poisson_iterations)
     else:
-        cpu.solve_with_cpu(
-            number_of_grid_steps, 
-            time_step_size, 
-            time_step_count, 
-            kinematic_viscosity, 
-            density, 
-            poisson_iterations
-            )
+        start = time.time()
+        p, u, v = cpu.solve(NX, DT, NT, nu, rho, poisson_iterations)
+        end = time.time()
+        print(end - start)
             
+    x = numpy.linspace(0, 1, NX)
+    y = numpy.linspace(0, 1, NX)
+    X, Y = numpy.meshgrid(x, y)
+    fig = pyplot.figure(figsize=(9, 7), dpi=100)
+    # plotting the pressure field as a contour
+    pyplot.contourf(X, Y, p, alpha=0.5, cmap=cm.viridis)  
+    pyplot.colorbar()
+    # plotting the pressure field outlines
+    pyplot.contour(X, Y, p, cmap=cm.viridis)  
+    # plotting velocity field
+    pyplot.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2]) 
+    pyplot.xlabel('X')
+    pyplot.ylabel('Y')
+    pyplot.show()
 
-    pass
+    fig = pyplot.figure(figsize=(9, 7), dpi=100)
+    pyplot.contourf(X, Y, p, alpha=0.5, cmap=cm.viridis)
+    pyplot.colorbar()
+    pyplot.contour(X, Y, p, cmap=cm.viridis)
+    pyplot.streamplot(X, Y, u, v)
+    pyplot.xlabel('X')
+    pyplot.ylabel('Y')
+    pyplot.show()
+    
 
 if __name__ == "__main__":
     main()
