4e0f328213f0d7d722cbd70d4be4780911ad0bb9
nando-2002 <prthik.kar@gmail.com>
2025-07-18 17:36:20 +0530

    Corrected main x,y swap error

--- src/cpu_solver.py
+++ src/cpu_solver.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from numba import jit, prange
 
-@jit(nopython = True, nogil = True, parallel = False)
+@jit(nopython = True, nogil = True, parallel = True)
 def bracket_terms(NX, DX, UN, VN, DT, rho):
 
     DY = DX
@@ -9,7 +9,7 @@ def bracket_terms(NX, DX, UN, VN, DT, rho):
     output = np.zeros((NX, NX))
     for i in prange(1, NX - 1, 1):
         for j in prange(1, NY - 1, 1):
-            output[i][j] = (
+            output[j][i] = (
             (
                 rho * (DX**2) * (DY**2) / (2*(DX**2 + DY**2))
             )  
@@ -20,26 +20,26 @@ def bracket_terms(NX, DX, UN, VN, DT, rho):
             )
             *
             (
-                (UN[i + 1][j] - UN[i - 1][j])/(2*DX) + (VN[i][j + 1] - VN[i][j - 1])/(2*DY)
+                (UN[j][i + 1] - UN[j][i - 1])/(2*DX) + (VN[j + 1][i] - VN[j - 1][i])/(2*DY)
             )
             -
             (
-                ((UN[i + 1][j] - UN[i - 1][j])/(2*DX))**2
+                ((UN[j][i + 1] - UN[j][i - 1])/(2*DX))**2
             )
             -
             (
-                2*((UN[i][j + 1] - UN[i][j - 1])/(2*DY))*((VN[i + 1][j] - VN[i - 1][j])/(2*DX))
+                2*((UN[j + 1][i] - UN[j - 1][i])/(2*DY))*((VN[j][i + 1] - VN[j][i - 1])/(2*DX))
             )
             -
             (
-                ((VN[i][j + 1] - VN[i][j - 1])/(2*DY))**2
+                ((VN[j + 1][i] - VN[j - 1][i])/(2*DY))**2
             )
             )
             )
         
     return output
 
-@jit(nopython = True, nogil = True, parallel = False)
+@jit(nopython = True, nogil = True, parallel = True)
 def pressure_poisson(P, DX, P_BRACKET, poisIT, NX):
 
     DY = DX
@@ -49,14 +49,14 @@ def pressure_poisson(P, DX, P_BRACKET, poisIT, NX):
         P_COPY = P.copy()
         for i in prange(1, NX - 1, 1):
             for j in prange(1, NY - 1, 1):
-                P[i][j] = (
+                P[j][i] = (
                     (
                     (
-                        (P_COPY[i + 1][j] + P_COPY[i - 1][j])*DY**2
+                        (P_COPY[j][i + 1] + P_COPY[j][i - 1])*DY**2
                     )
                     +
                     (
-                        (P_COPY[i][j + 1] + P_COPY[i][j - 1])*DX**2
+                        (P_COPY[j + 1][i] + P_COPY[j - 1][i])*DX**2
                     )
                     )
                     /
@@ -65,19 +65,19 @@ def pressure_poisson(P, DX, P_BRACKET, poisIT, NX):
                     )
                     -
                     (
-                        P_BRACKET[i][j]
+                        P_BRACKET[j][i]
                     )
                     )
         
-        P[:, -1] = P[:, -2] # dp/dx = 0 at x = 1
-        P[0, :] = P[1, :]   # dp/dy = 0 at y = 0
-        P[:, 0] = P[:, 1]   # dp/dx = 0 at x = 0
-        P[-1, :] = 0       # p = 0 at y = 1
+        P[:, -1] = P[:, -2] # dP/dx = 0 at x = 2
+        P[0, :] = P[1, :]   # dP/dy = 0 at y = 0
+        P[:, 0] = P[:, 1]   # dP/dx = 0 at x = 0
+        P[-1, :] = 0        # P = 0 at y = 2
     
     return P
 
 
-@jit(nopython = True, nogil = True, parallel = False)
+@jit(nopython = True, nogil = True, parallel = True)
 def solve(NX, DT, NT, nu, rho, poisIT):
 
     NY = NX
@@ -102,21 +102,21 @@ def solve(NX, DT, NT, nu, rho, poisIT):
 
         for i in prange(1, NX - 1, 1):
             for j in prange(1, NY - 1, 1):   
-               VEL_NEXT_U[i][j] = (
+               VEL_NEXT_U[j][i] = (
                (
-                    VEL_NOW_U[i][j]
+                    VEL_NOW_U[j][i]
                )
                +
                (
-                   - VEL_NOW_U[i][j]*(DT/DX)*(VEL_NOW_U[i][j] - VEL_NOW_U[i - 1][j])
+                   - VEL_NOW_U[j][i]*(DT/DX)*(VEL_NOW_U[j][i] - VEL_NOW_U[j - 1][i])
                )
                +
                (
-                   - VEL_NOW_V[i][j]*(DT/DY)*(VEL_NOW_U[i][j] - VEL_NOW_U[i][j - 1])
+                   - VEL_NOW_V[j][i]*(DT/DY)*(VEL_NOW_U[j][i] - VEL_NOW_U[j][i - 1])
                )
                +
                (
-                   (-DT/(rho*2*DX))*(PRES_NOW[i + 1][j] - PRES_NOW[i - 1][j]) 
+                   (-DT/(rho*2*DX))*(PRES_NOW[j][i + 1] - PRES_NOW[j][i - 1]) 
                )
                +
                (
@@ -126,33 +126,33 @@ def solve(NX, DT, NT, nu, rho, poisIT):
                        (DT/DX**2)
                        *
                        (
-                           VEL_NOW_U[i + 1][j] - 2*VEL_NOW_U[i][j] + VEL_NOW_U[i - 1][j]
+                           VEL_NOW_U[j][i + 1] - 2*VEL_NOW_U[j][i] + VEL_NOW_U[j][i - 1]
                        )
                        +
                        (DT/DX**2)
                        *
                        (
-                           VEL_NOW_U[i][j + 1] - 2*VEL_NOW_U[i][j] + VEL_NOW_U[i][j - 1]
+                           VEL_NOW_U[j + 1][i] - 2*VEL_NOW_U[j][i] + VEL_NOW_U[j - 1][i]
                        )
                    )
                )
                )
 
-               VEL_NEXT_V[i][j] = (
+               VEL_NEXT_V[j][i] = (
                (
-                    VEL_NOW_V[i][j]
+                    VEL_NOW_V[j][i]
                )
                +
                (
-                   - VEL_NOW_U[i][j]*(DT/DX)*(VEL_NOW_V[i][j] - VEL_NOW_V[i - 1][j])
+                   - VEL_NOW_U[j][i]*(DT/DX)*(VEL_NOW_V[j][i] - VEL_NOW_V[j - 1][i])
                )
                +
                (
-                   - VEL_NOW_V[i][j]*(DT/DY)*(VEL_NOW_V[i][j] - VEL_NOW_V[i][j - 1])
+                   - VEL_NOW_V[j][i]*(DT/DY)*(VEL_NOW_V[j][i] - VEL_NOW_V[j][i - 1])
                )
                +
                (
-                   (-DT/(rho*2*DX))*(PRES_NOW[i][j + 1] - PRES_NOW[i][j - 1]) 
+                   (-DT/(rho*2*DX))*(PRES_NOW[j + 1][i] - PRES_NOW[j - 1][i]) 
                )
                +
                (
@@ -162,13 +162,13 @@ def solve(NX, DT, NT, nu, rho, poisIT):
                        (DT/DX**2)
                        *
                        (
-                           VEL_NOW_V[i + 1][j] - 2*VEL_NOW_V[i][j] + VEL_NOW_V[i - 1][j]
+                           VEL_NOW_V[j][i + 1] - 2*VEL_NOW_V[j][i] + VEL_NOW_V[j][i - 1]
                        )
                        +
                        (DT/DX**2)
                        *
                        (
-                           VEL_NOW_V[i][j + 1] - 2*VEL_NOW_V[i][j] + VEL_NOW_V[i][j - 1]
+                           VEL_NOW_V[j + 1][i] - 2*VEL_NOW_V[j][i] + VEL_NOW_V[j - 1][i]
                        )
                    )
                )
@@ -177,7 +177,7 @@ def solve(NX, DT, NT, nu, rho, poisIT):
         VEL_NEXT_U[0, :]  = 0
         VEL_NEXT_U[:, 0]  = 0
         VEL_NEXT_U[:, -1] = 0
-        VEL_NEXT_U[-1, :] = 1   # set velocity on cavity lid equal to 1
+        VEL_NEXT_U[-1, :] = 1    # set velocity on cavity lid equal to 1
         VEL_NEXT_V[0, :]  = 0
         VEL_NEXT_V[-1, :] = 0
         VEL_NEXT_V[:, 0]  = 0
